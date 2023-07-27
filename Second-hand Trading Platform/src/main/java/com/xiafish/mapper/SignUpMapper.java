83daa11c955ab6bc3292b95921b55a0b0f043973
x111 <1209653508@qq.com>
2023-07-27 17:14:15 +0800

    修改了bug

--- Second-hand Trading Platform/src/main/java/com/xiafish/mapper/SignUpMapper.java
+++ Second-hand Trading Platform/src/main/java/com/xiafish/mapper/SignUpMapper.java
@@ -7,8 +7,8 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface SignUpMapper {
 
-    @Insert("insert into xiafish.user(user_name,user_passwd) values(#{username},#{password})")
-    void addUser(String username, String password);
+    @Insert("insert into xiafish.user(user_name,user_passwd,user_phone_num,user_email) values(#{username},#{password},#{phone},#{email})")
+    void addUser(String username, String password,String phone,String email);
 
     @Select("SELECT LAST_INSERT_ID()")
     Integer selectId();
